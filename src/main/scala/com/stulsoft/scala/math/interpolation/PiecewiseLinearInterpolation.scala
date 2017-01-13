1a81e9d8e8bd44c14f64d250a7d26193442753d2
ysden123 <ysden123@gmail.com>
2017-01-13 23:12:23 +0200

    validate

--- src/main/scala/com/stulsoft/scala/math/interpolation/PiecewiseLinearInterpolation.scala
+++ src/main/scala/com/stulsoft/scala/math/interpolation/PiecewiseLinearInterpolation.scala
@@ -7,9 +7,58 @@ package com.stulsoft.scala.math.interpolation
 /**
   * Y = Y0 + (X - X0)*(Y1 - Y0)/(X1 - X0)
   *
+  * @param points sequence of points (x, y)
   * @author Yuriy Stul
   */
 case class PiecewiseLinearInterpolation(points: Seq[(Double, Double)]) {
   require(points != null, "The points must be specified.")
   require(points.size > 1, "The points must contain at least two items.")
+
+  // Validate input sequence
+  points.tail
+    .foldLeft(points.head._1) {
+      (prev, current) => {
+        if (current._1 <= prev)
+          throw new IllegalArgumentException("Unsorted input sequence")
+        else
+          current._1
+      }
+    }
+
+  /**
+    * Computes a interpolated value
+    *
+    * @param x argument
+    * @return the interpolated value
+    */
+  def get(x: Double): Double = {
+    val startAndEndX = getBoundPoints(x)
+
+    if (startAndEndX.isEmpty) {
+      val lastPoint = points.reverse.head
+      if (Math.abs(lastPoint._1 - x) < 0.000001)
+        return lastPoint._2
+      else
+        throw new IllegalArgumentException(s"Argument $x is out of boundary [${points.head._1} - ${points.reverse.head._1}]")
+    }
+
+    (startAndEndX.get._1._2
+      + (x - startAndEndX.get._1._1) * (startAndEndX.get._2._2 - startAndEndX.get._1._2)
+      / (startAndEndX.get._2._1 - startAndEndX.get._1._1))
+  }
+
+  private def getBoundPoints(x: Double): Option[((Double, Double), (Double, Double))] = {
+    val xStart = points.reverse
+      .find(p => p._1 <= x)
+
+    if (xStart.isDefined) {
+      val xEnd = points.find(p => p._1 > x)
+      if (xEnd.isDefined)
+        Option(Tuple2(xStart.get, xEnd.get))
+      else
+        Option.empty
+    } else {
+      Option.empty
+    }
+  }
 }
--- src/test/scala/com/stulsoft/scala/math/interpolation/PiecewiseLinearInterpolationTest.scala
+++ src/test/scala/com/stulsoft/scala/math/interpolation/PiecewiseLinearInterpolationTest.scala
@@ -11,6 +11,7 @@ import org.scalatest.{FlatSpec, Matchers}
   */
 class PiecewiseLinearInterpolationTest extends FlatSpec with Matchers {
   behavior of "PiecewiseLinearInterpolation"
+
   "constructor" should "initialize instance of the PiecewiseLinearInterpolation class" in {
     val c = PiecewiseLinearInterpolation(Seq((1.0, 2.0), (2.0, 4.0), (3.0, 9.0)))
     c.points.toString should be("List((1.0,2.0), (2.0,4.0), (3.0,9.0))")
@@ -24,7 +25,32 @@ class PiecewiseLinearInterpolationTest extends FlatSpec with Matchers {
 
   it should "prevent usage of list of points with number of items less than 2" in {
     assertThrows[IllegalArgumentException] {
-      PiecewiseLinearInterpolation(null)
+      PiecewiseLinearInterpolation(Seq((2, 1)))
+    }
+  }
+
+  it should "prevent usage of unsorted sequence" in {
+    assertThrows[IllegalArgumentException] {
+      PiecewiseLinearInterpolation(Seq((2, 1), (1, 2), (3, 4)))
+    }
+  }
+
+  "get" should "compute value" in {
+    val c = PiecewiseLinearInterpolation(Seq((1.0, 1.0), (2.0, 2.0), (3.0, 3.0)))
+    c.get(1.0) should be(1.0)
+    c.get(1.5) should be(1.5)
+    c.get(2.0) should be(2.0)
+    c.get(2.9) should be(2.9)
+    c.get(3.0) should be(3.0)
+  }
+
+  it should "throw exception for x outside boundary" in {
+    val c = PiecewiseLinearInterpolation(Seq((1.0, 1.0), (2.0, 2.0), (3.0, 3.0)))
+    assertThrows[IllegalArgumentException] {
+      c.get(0.5)
+    }
+    assertThrows[IllegalArgumentException] {
+      c.get(3.001)
     }
   }
 }
