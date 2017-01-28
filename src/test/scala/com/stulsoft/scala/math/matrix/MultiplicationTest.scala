c003826bc8bca70b80ac4037c50e2a8428f77320
ysden123 <ysden123@gmail.com>
2017-01-28 17:31:37 +0200

    matrix multiplication

--- src/test/scala/com/stulsoft/scala/math/matrix/MultiplicationTest.scala
+++ src/test/scala/com/stulsoft/scala/math/matrix/MultiplicationTest.scala
@@ -33,4 +33,52 @@ class MultiplicationTest extends FlatSpec with Matchers {
 
     Multiplication.simpleProduct(Array.ofDim[Double](7, 8), Array.ofDim[Double](8, 7))
   }
+
+  it should "compute product of two square matrices" in {
+    val a = Array(Array(1.0, 2.0),
+      Array(2.0, 3.0))
+    val b = Array(Array(10.0, 20.0),
+      Array(20.0, 30.0))
+    Multiplication.simpleProduct(a, b) should equal(Multiplication.simpleProduct(b, a))
+  }
+
+  it should "compute product of two rectangle matrices" in {
+    val a = Array(Array(-1.0, 1.0),
+      Array(2.0, 0.0),
+      Array(0.0, 3.0))
+    val b = Array(Array(3.0, 1.0, 2.0),
+      Array(0.0, -1.0, 4.0))
+
+    //    displayMatrix(Multiplication.simpleProduct(a, b))
+    //    displayMatrix(Multiplication.simpleProduct(b, a))
+    Multiplication.simpleProduct(a, b) should equal(Array(
+      Array(-3.0, -2.0, 2.0),
+      Array(6.0, 2.0, 4.0),
+      Array(0.0, -3.0, 12.0)
+    ))
+    Multiplication.simpleProduct(b, a) should equal(Array(
+      Array(-1.0, 9.0),
+      Array(-2.0, 12.0)))
+  }
+
+  it should "compute product of matrix and vector" in {
+    val a = Array(
+      Array(2.0, 0.0, 4.0, -1.0),
+      Array(1.0, -1.0, 1.0, 0.0))
+    val b = Array(
+      Array(2.0),
+      Array(1.0),
+      Array(0.0),
+      Array(-2.0))
+    Multiplication.simpleProduct(a, b) should equal(Array(
+      Array(6.0),
+      Array(1.0)
+    ))
+  }
+
+  def displayMatrix(a: Array[Array[Double]]): Unit = {
+    a.indices.foreach(i => {
+      println(s"""[${a(i).mkString(", ")}]""")
+    })
+  }
 }
