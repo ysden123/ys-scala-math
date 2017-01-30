27347d36ec3aefb6f2ed9bc0dc8a9241112b0f40
ysden123 <ysden123@gmail.com>
2017-01-30 21:53:32 +0200

    double

--- src/main/scala/com/stulsoft/scala/math/matrix/Matrix.scala
+++ src/main/scala/com/stulsoft/scala/math/matrix/Matrix.scala
@@ -7,45 +7,23 @@ package com.stulsoft.scala.math.matrix
 import scala.reflect.ClassTag
 
 /**
-  * Matrix operations
+  * Matrix operations.
   *
+  * @param m matrix
   * @author Yuriy Stul
   */
-case class Matrix[T: ClassTag](m: Array[Array[T]]) {
+class Matrix[T: ClassTag](val m: Array[Array[T]]) {
   require(m != null, "m must be specified")
   require(m.length != 0, "m must have at least 1 element")
 
-  override def equals(obj: scala.Any): Boolean = {
-    this.m.deep == obj.asInstanceOf[Matrix[T]].m.deep
-  }
+  override def equals(obj: scala.Any): Boolean = this.m.deep == obj.asInstanceOf[Matrix[T]].m.deep
 
   override def toString: String = {
-    val b: StringBuilder = new StringBuilder("Matrix[T]: ")
+    val b: StringBuilder = new StringBuilder("MatrixT[T]: ")
     m.indices.foreach(i => {
       b.append( s"""[${m(i).mkString(", ")}]""")
     })
     b.toString
-  }
-
-  /**
-    * Multiplication
-    *
-    * @param that multiplier
-    * @return product of this matrix on that matrix
-    */
-  def *(that: Matrix[Double]): Matrix[Double] = {
-    require(m(0).length == that.m.length, "Number of columns in 1st matrix (m) must be equal to number of rows in second matrix (that)")
-
-    // Initialize result matrix
-    val c = Array.fill[Double](m.length, that.m(0).length)(0.0)
-    m.indices.foreach(i => {
-      that.m(0).indices.foreach(j => {
-        m(0).indices.foreach(r => {
-          c(i)(j) += m(i)(r).asInstanceOf[Double] * that.m(r)(j)
-        })
-      })
-    })
-    Matrix(c)
   }
 
   /**
@@ -58,6 +36,48 @@ case class Matrix[T: ClassTag](m: Array[Array[T]]) {
     m.indices.foreach(i =>
       m(0).indices.foreach(j => r(j)(i) = m(i)(j))
     )
-    Matrix(r)
+    new Matrix[T](r)
   }
+}
+
+/**
+  * Matrix arithmetic
+  *
+  * @param m matrix
+  */
+class MatrixDouble(m: Array[Array[Double]]) extends Matrix(m: Array[Array[Double]]) {
+  /**
+    * Multiplication
+    *
+    * @param that multiplier
+    * @return product of this matrix on that matrix
+    */
+  def *(that: MatrixDouble): MatrixDouble = {
+    require(m(0).length == that.m.length, "Number of columns in 1st matrix (m) must be equal to number of rows in second matrix (that)")
+
+    // Initialize result matrix
+    val c = Array.fill[Double](m.length, that.m(0).length)(0.0)
+    m.indices.foreach(i => {
+      that.m(0).indices.foreach(j => {
+        m(0).indices.foreach(r => {
+          c(i)(j) += m(i)(r) * that.m(r)(j)
+        })
+      })
+    })
+    new MatrixDouble(c)
+  }
+}
+
+object Test extends App {
+  val m = new Matrix[Int](Array(
+    Array(1, 2, 3),
+    Array(10, 20, 30)
+  ))
+  println(m)
+
+  val mDouble = new MatrixDouble(Array(
+    Array(1.0, 2.0, 3.0),
+    Array(10.0, 20.0, 30.0)
+  ))
+  println(mDouble)
 }
